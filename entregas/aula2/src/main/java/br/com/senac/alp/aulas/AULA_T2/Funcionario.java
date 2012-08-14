28fce79d14259173498a7e78fdb9dbf70ee844b4
jpfflores <arokhyn@gmail.com>
2012-08-13 22:31:23 -0300

    Adicionando no diretório correto.

---
+++ entregas/aula2/src/main/java/br/com/senac/alp/aulas/AULA_T2/Funcionario.java
@@ -0,0 +1,15 @@
+package br.com.senac.alp.aulas.AULA_T2;
+
+public class Funcionario extends Pessoa{
+
+		private Integer id = null;
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		} 
+		
+}

129cf0a8fbc1742c8f2da20936a0f7949703fc7a
jpfflores <arokhyn@gmail.com>
2012-08-13 21:48:06 -0300

    Adicionando as classe da aula 02.

---
+++ entregas/AULA_T2/src/main/java/br/com/senac/alp/aulas/AULA_T2/Funcionario.java
@@ -0,0 +1,7 @@
+package br.com.senac.alp.aulas.AULA_T2;
+
+public class Funcionario extends Pessoa{
+
+		private Integer id = null; 
+		
+}
---
+++ entregas/AULA_T2/src/main/java/br/com/senac/alp/aulas/AULA_T2/Secretaria.java
@@ -0,0 +1,5 @@
+package br.com.senac.alp.aulas.AULA_T2;
+
+public class Secretaria extends Funcionario{
+
+}

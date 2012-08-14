8dc41344160af310c436db01b9fbb3c895b4096d
jpfflores <arokhyn@gmail.com>
2012-08-13 22:22:05 -0300

    Classes completas para sexta.

--- entregas/AULA_T2/src/main/java/br/com/senac/alp/aulas/AULA_T2/Funcionario.java
+++ entregas/AULA_T2/src/main/java/br/com/senac/alp/aulas/AULA_T2/Funcionario.java
@@ -2,6 +2,14 @@ package br.com.senac.alp.aulas.AULA_T2;
 
 public class Funcionario extends Pessoa{
 
-		private Integer id = null; 
+		private Integer id = null;
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		} 
 		
 }

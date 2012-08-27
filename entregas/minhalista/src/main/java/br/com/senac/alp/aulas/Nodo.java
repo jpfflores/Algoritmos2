363cb3606bc7f5b909de8f9c9e01dbfb96228c7f
jpfflores <arokhyn@gmail.com>
2012-08-27 19:36:00 -0300

    Mudando nomes.

--- entregas/minhalista/src/main/java/br/com/senac/alp/aulas/Nodo.java
+++ entregas/minhalista/src/main/java/br/com/senac/alp/aulas/Nodo.java
@@ -2,7 +2,7 @@ package br.com.senac.alp.aulas;
 
 public class Nodo<Tipo extends Object> {
 	private Tipo valor;
-	private Nodo proximo = null;
+	private Nodo<Tipo> proximo = null;
 	
 	public void setProximo(Nodo<Tipo> left){
 		this.proximo = left;
@@ -10,7 +10,7 @@ public class Nodo<Tipo extends Object> {
 	public Tipo getValor(){
 		return this.valor;
 	}
-	public Nodo getProximo() {
+	public Nodo<Tipo> getProximo() {
 		return this.proximo;
 	}
 	public void setValor(Tipo valor) {

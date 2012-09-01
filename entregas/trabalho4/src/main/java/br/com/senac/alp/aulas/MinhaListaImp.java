9e664ab089d659591e8b715bb167bb29f928f4c7
jpfflores <arokhyn@gmail.com>
2012-08-31 22:35:13 -0300

    Correção de incialização.

--- entregas/trabalho4/src/main/java/br/com/senac/alp/aulas/MinhaListaImp.java
+++ entregas/trabalho4/src/main/java/br/com/senac/alp/aulas/MinhaListaImp.java
@@ -1,54 +1,37 @@
 package br.com.senac.alp.aulas;
 
-
-public class MinhaListaImp<Tipo> implements MinhaLista<Tipo>{
+public class MinhaListaImp<Tipo> implements MinhaLista<Tipo> {
 
 	private Nodo<Tipo> inicio = null;
-	
-	public MinhaListaImp()
-	{
-		Tipo obj = (Tipo) new Object();
+
+	public MinhaListaImp() {
+		Tipo obj = null;
 		this.inicio = new Nodo<Tipo>(obj);
-		
+
 	}
-	
-	public MinhaListaImp(Tipo valorinicio)
-	{
-		if(valorinicio == null){
-			throw new IllegalArgumentException();
-		}
-		
-		Tipo obj = (Tipo) new Object();
-		this.inicio = new Nodo<Tipo>(obj);
-		
-		Nodo<Tipo> nodo = new Nodo<Tipo>(valorinicio);
-		this.inicio.setProximo(nodo);
-			
-	}
+
 	/*
-	protected void setInicio(Nodo<Tipo> valor){
-		this.inicio = valor;
-	}
-	*/
-	
-	protected Nodo<Tipo> getInicio(){
+	 * protected void setInicio(Nodo<Tipo> valor){ this.inicio = valor; }
+	 */
+
+	protected Nodo<Tipo> getInicio() {
 		return this.inicio;
 	}
-	
+
 	public void sufixar(Tipo valor) {
 		Nodo<Tipo> ultimo = ultimoElemento();
 		Nodo<Tipo> novoUltimo = new Nodo<Tipo>(valor);
-		ultimo.setProximo(novoUltimo);		
+		ultimo.setProximo(novoUltimo);
 	}
 
 	public void prefixar(Tipo valor) {
 		Nodo<Tipo> primeiro = buscarPrimeiroNodo();
 		Nodo<Tipo> novoPrimeiro = new Nodo<Tipo>(valor);
 		Nodo<Tipo> fantasma = getInicio();
-		if(primeiro != null){
+		if (primeiro != null) {
 			novoPrimeiro.setProximo(primeiro);
 		}
-		fantasma.setProximo(novoPrimeiro);		
+		fantasma.setProximo(novoPrimeiro);
 	}
 
 	public Tipo buscar(int pos) {
@@ -57,7 +40,7 @@ public class MinhaListaImp<Tipo> implements MinhaLista<Tipo>{
 	}
 
 	public void inserir(int pos, Tipo valor) {
-		Nodo<Tipo> nodoAnt = buscarNodo(pos -1);
+		Nodo<Tipo> nodoAnt = buscarNodo(pos - 1);
 		Nodo<Tipo> nodo = new Nodo<Tipo>(valor);
 		Nodo<Tipo> velhoNodoPos = nodoAnt.getProximo();
 		nodoAnt.setProximo(nodo);
@@ -65,7 +48,7 @@ public class MinhaListaImp<Tipo> implements MinhaLista<Tipo>{
 	}
 
 	public Tipo remover(int pos) {
-		Nodo<Tipo> nodoA = buscarNodo(pos -1);
+		Nodo<Tipo> nodoA = buscarNodo(pos - 1);
 		Nodo<Tipo> nodoB = nodoA.getProximo();
 		Nodo<Tipo> nodoC = nodoB.getProximo();
 		nodoA.setProximo(nodoC);
@@ -75,39 +58,37 @@ public class MinhaListaImp<Tipo> implements MinhaLista<Tipo>{
 	public int tamanho() {
 		int tamanho = 0;
 		Nodo<Tipo> nodo = this.getInicio();
-		while(nodo.getProximo()!= null)
-		{
+		while (nodo.getProximo() != null) {
 			nodo = nodo.getProximo();
 			tamanho++;
 		}
 		return tamanho;
 	}
 
-	private Nodo<Tipo> ultimoElemento(){
-		int ultimaPos = tamanho() -1;
+	private Nodo<Tipo> ultimoElemento() {
+		int ultimaPos = tamanho() - 1;
 		Nodo<Tipo> ultimo = buscarNodo(ultimaPos);
 		return ultimo;
 	}
-	
-	private Nodo<Tipo> buscarNodo(int pos){
+
+	private Nodo<Tipo> buscarNodo(int pos) {
 		Nodo<Tipo> nodo = getInicio();
-		if(nodo.getProximo() == null){
+		if (nodo.getProximo() == null) {
 			throw new IndexOutOfBoundsException("Lista vazia.");
 		}
-		if(pos > tamanho()){
-			throw new IndexOutOfBoundsException("Posição maior que o tamanho da Lista");
+		if (pos > tamanho()) {
+			throw new IndexOutOfBoundsException(
+					"Posição maior que o tamanho da Lista");
 		}
-		for (int i = 0; i < pos ; i++)
-		{
+		for (int i = 0; i < pos; i++) {
 			nodo = nodo.getProximo();
 		}
 		return nodo;
 	}
-	
-	private Nodo<Tipo> buscarPrimeiroNodo()
-	{
+
+	private Nodo<Tipo> buscarPrimeiroNodo() {
 		Nodo<Tipo> resultado = this.inicio.getProximo();
 		return resultado;
 	}
-	
+
 }

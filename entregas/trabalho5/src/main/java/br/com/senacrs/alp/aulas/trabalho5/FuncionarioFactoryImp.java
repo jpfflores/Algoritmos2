5b101ecdcdaaa694648aaaa06f3a48facfc97569
jpfflores <arokhyn@gmail.com>
2012-09-10 22:34:27 -0300

    Criando o trabalho5.

---
+++ entregas/trabalho5/src/main/java/br/com/senacrs/alp/aulas/trabalho5/FuncionarioFactoryImp.java
@@ -0,0 +1,11 @@
+package br.com.senacrs.alp.aulas.trabalho5;
+
+public class FuncionarioFactoryImp implements FuncionarioFactory {
+
+	@Override
+	public Funcionario criaFuncionario(String nome, int salario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ entregas/trabalho5/src/main/java/br/com/senacrs/alp/aulas/trabalho5/FuncionarioImp.java
@@ -0,0 +1,16 @@
+package br.com.senacrs.alp.aulas.trabalho5;
+
+public class FuncionarioImp implements Funcionario, Pessoa{
+
+
+	private String nome = null;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
